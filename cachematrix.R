ddaa1f64f705a4f9c0ac7b1f55a2a6a2b758e17e
Simon Mark Burt <simon.burt@wds.co>
2014-07-22 07:21:15 +0100

    First commit
    first commit stub

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## commit 1 stub
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
